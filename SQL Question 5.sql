93f9f2706780ee7d4bc918cc949e9411984e1629
indavishaurya <126399457+indavishaurya@users.noreply.github.com>
2023-02-26 20:19:33 +0530

    Delete SQL Question 5.sql

--- SQL Question 5.sql
+++
@@ -1,3 +0,0 @@
-Select distinct [year],count(country_code) over(partition by [year]) as count_countries,round(sum(gdp_per_capita) over(partition by [year]),2) as sum_countries
-from per_capita
-where [year]<2012 and country_code in (Select country_code from per_capita where [year]=2012 and gdp_per_capita is NULL) and gdp_per_capita is not null
